48538bfbe53d1609c9b2a4caa21519aceff68636
Gokulakrishnan Shankar <gokullan1101@gmail.com>
2024-01-15 19:58:47 +0530

    Removed API Key

--- notes-service/routes/routes_handlers.js
+++ notes-service/routes/routes_handlers.js
@@ -1,6 +1,6 @@
 const db = require('../database/db');
 const rmq = require('../send.js');
-const api_key = "AIzaSyA6T9rtYjW2tEd_YvIEhcpzjGTUyc9jCc0";
+const api_key = "YOUR_API_KEY";
 const redis = require("redis");
 
 let redisClient;
